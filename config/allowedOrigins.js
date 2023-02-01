6f9b20cebd4ce06e4d7b500fe25ab208f71e7ab1
Kabir <44284877+kabir0x23@users.noreply.github.com>
2023-02-02 01:26:49 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,5 +2,6 @@ module.exports = [
     'https://thecyberhub.org',
     'https://beta.thecyberhub.org',
     'https://dev.thecyberhub.org',
+    'https://dev.api.thecyberhub.org/',
     'http://localhost:3000',
 ];
